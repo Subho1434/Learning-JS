97a556ea9d924d31f1ea3b122d0796165ce12747
SUBANKAR DEY <subankardey1434@gmail.com>
2024-12-23 18:56:38 +0000

    premetive data types

---
+++ basic_js/datatypes.js
@@ -0,0 +1,48 @@
+//Premetive Data Types
+
+/*
+1. Number
+2. BigInt
+3. String 
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+*/
+
+let age = 23 //Number
+let pi = 3.14 //Float-Point Number
+let infinityValue = Infinity //Special Numeric value
+let notANumber = NaN //Not a Number
+console.log(typeof(age))
+
+let num = 9876543210n // Big int 2^53 - 1 =
+console.log(typeof(num))
+
+let uname = "Subankar Dey"
+console.log(typeof(uname))
+
+const state = true
+console.log(typeof(state));
+
+let email
+console.log(typeof(email));
+
+//Non Premetive Data Types
+
+/*
+1. Object
+2. Array
+3. Function 
+4. Date
+5. Map
+6. Set
+*/
+
+//Dynamic Typing
+
+// let value = 1234
+// value = "subho@143"
+
+// console.log(typeof(value))
+// console.log(value);
